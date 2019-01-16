7e74f03af5aaa9e1c2062d7128cf03b25de55afc
Your Name <you@example.com>
2019-01-16 14:09:21 -0800

    Correcting population logic.

--- js/main.js
+++ js/main.js
@@ -1,37 +1,44 @@
-let mealItem = "";
-let mealCourse = "";
+let mealType = "";
 let breakfastFood = ["Eggs", "Bacon", "Toast"];
 let lunchFood = ["Hamburger", "Pizza", "Milkshake", "Fries"];
 let dinnerFood = ["Steak", "Mash Potato", "Carrots", "Soda", "Corn"];
 
-const dropdown = {
+const yummm = {
     main: () => {
-        dropdown.addListener();
+        yummm.setListener();
     },
-    addListener: (n) => {
+    setListener: (n) => {
         document.getElementsByClassName("meal-container")[0].addEventListener("change", function() {
-            mealItem = this.value;
-            dropdown.setOptions();
+            mealType = this.value.toLowerCase();
+            yummm.setOptions();
         }, false);
     },
+    getArr: () => {
+        if(mealType==="breakfast") {
+            return breakfastFood;
+        } else if(mealType==="lunch") {
+            return lunchFood;
+        } else if(mealType==="dinner") {
+            return dinnerFood;
+        } else {
+            console.log("ELSE");
+        }
+    },
     getFoodLength: () => {
-        let arr = mealItem + "Food";
-        console.log("---", arr);
-        return arr.length;
+        return yummm.getArr().length;
     },
     setOptions: () => {
-        let len = dropdown.getFoodLength();
-        let mealCourse = mealItem + "Food";
+        let len = yummm.getFoodLength();
 
         for(let i=0; i <len; i++) {
             let option = document.createElement("option");
-            option.setAttribute("value", mealCourse[i].toLowerCase());
+            option.setAttribute("value", yummm.getArr()[i].toLowerCase());
             option.setAttribute("class", "food");
-            let copy = document.createTextNode(mealCourse[i]);
+            let copy = document.createTextNode(yummm.getArr()[i]);
             option.appendChild(copy);
             document.getElementsByClassName("food-container")[0].appendChild(option);
         }
     }
 }
 
-dropdown.main();
+yummm.main();
